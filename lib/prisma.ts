1c569c9ad5c58fd02a67d6962aea4b20fff5320f
bruno-henrique-crtl <bruno.henriqueds@outlook.com.br>
2026-05-20 18:58:14 -0300

    first commit

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "../prisma/generated/client"
 import { PrismaLibSql } from "@prisma/adapter-libsql"
 
 const adapter = new PrismaLibSql({ url: process.env.DATABASE_URL!, authToken: process.env.AUTH_TOKEN! })
